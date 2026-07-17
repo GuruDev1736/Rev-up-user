b2d321cb05fc538b20c38cf27cb194de66d65935
guruprasad <guruprasad1736@gmail.com>
2026-07-18 00:10:33 +0530

    Refactor code structure for improved readability and maintainability

---
+++ android/app/src/main/java/com/revupbikes/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.revupbikes.app;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
